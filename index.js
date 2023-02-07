fd34086b038fdb33be84600849d0487473270fc3
Nick Salloum <nick@callmenick.com>
2023-02-07 19:50:04 -0400

    updates dom logic and presentation based on simplified tictactoe

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-import {
-  start,
-  play,
-  replay,
-  reset,
-  getState,
-  getCurrentPlayer,
-} from './tictactoe.js'
+import { play, reset, getState, getCurrentPlayer } from './tictactoe.js'
 
 function main() {
   // get various elements in the dom
   const $board = document.querySelector('.board')
   const $boardCells = document.querySelectorAll('.board-cell')
   const $currentPlayer = document.querySelector('.current-player')
-  const $startButton = document.querySelector('.button-start')
-  const $replayButton = document.querySelector('.button-replay')
   const $resetButton = document.querySelector('.button-reset')
 
   // handle cell clicks
   function onCellClick($element, index) {
-    let state = getState()
-    let currentPlayer = getCurrentPlayer()
+    // get state and current player before the play is made
+    const stateBeforePlay = getState()
+    const currentPlayerBeforePlay = getCurrentPlayer()
 
-    // don't bother with dom logic if the game is not in a playing state
-    if (state !== 'playing') return
+    // halt execution if the game is not in a playing state
+    if (stateBeforePlay !== 'playing') return
 
     // get the row and col based on cell index
     const row = Math.floor(index / 3)
     const col = index % 3
 
     try {
-      // make the play
+      // attempt to make the play
       play(row, col)
 
-      // update the cell with the current player
-      $element.innerHTML = currentPlayer
+      // update the cell with the player who just played
+      $element.innerHTML = currentPlayerBeforePlay
       $element.classList.add('filled')
 
-      state = getState()
+      // get the state and current player after the play is made
+      const stateAfterPlay = getState()
+      const currentPlayerAfterPlay = getCurrentPlayer()
 
-      // decide what to do depending on the next state
-      switch (state) {
+      // decide what to do depending on state after play
+      switch (stateAfterPlay) {
         case 'win': {
-          alert(`player ${currentPlayer} wins!`)
+          // run this in a setTimeout so the dom gets filled before the alert
+          // pops up on screen
+          setTimeout(() => alert(`player ${currentPlayerBeforePlay} wins!`), 0)
           break
         }
         case 'draw': {
@@ -49,8 +45,7 @@ function main() {
           break
         }
         case 'playing': {
-          currentPlayer = getCurrentPlayer()
-          $currentPlayer.innerHTML = currentPlayer
+          $currentPlayer.innerHTML = currentPlayerAfterPlay
           break
         }
         default:
@@ -61,54 +56,23 @@ function main() {
     }
   }
 
-  // handle game start
-  function onStart() {
-    start()
-    $currentPlayer.innerHTML = getCurrentPlayer()
-    $board.classList.add('board-active')
-    $board.classList.remove('board-inactive')
-    $startButton.setAttribute('disabled', true)
-    $replayButton.removeAttribute('disabled')
-    $resetButton.removeAttribute('disabled')
-  }
-
-  // handle game replay
-  function onReplay() {
-    replay()
-    $boardCells.forEach((cell) => (cell.innerHTML = ''))
-    $currentPlayer.innerHTML = getCurrentPlayer()
-    $startButton.removeAttribute('disabled')
-    $replayButton.setAttribute('disabled', true)
-    $resetButton.setAttribute('disabled', true)
-  }
-
   // handle game reset
   function onReset() {
     reset()
     $board.classList.add('board-inactive')
     $board.classList.remove('board-active')
     $boardCells.forEach((cell) => (cell.innerHTML = ''))
-    $currentPlayer.innerHTML = '-'
-    $startButton.removeAttribute('disabled')
-    $replayButton.setAttribute('disabled', true)
-    $resetButton.setAttribute('disabled', true)
+    $currentPlayer.innerHTML = getCurrentPlayer()
   }
+
+  // add currentPlayer to dom
+  $currentPlayer.innerHTML = getCurrentPlayer()
 
   // handle click event for board cells
   $boardCells.forEach((element, index) => {
     element.addEventListener('click', () => {
       onCellClick(element, index)
     })
-  })
-
-  // handle click event for start button
-  $startButton.addEventListener('click', () => {
-    onStart()
-  })
-
-  // handle click event for replay button
-  $replayButton.addEventListener('click', () => {
-    onReplay()
   })
 
   // handle click event for reset button
